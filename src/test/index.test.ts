e98e1eb6dbd545476158c57c611b1d970a94a2cb
mikkurogue <michael.lindemans@outlook.com>
2025-05-20 20:56:03 +0200

    add test for fromnullable

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,15 +1,20 @@
-import { ifSome, isSome, match, Some } from "..";
+import { fromNullable, ifSome, isSome, match, Some } from "..";
 import { test, expect } from "vitest";
 
 test("Some holds value", () => {
-  const value = Some(123);
-  expect(isSome(value)).toBe(true);
+    const value = Some(123);
+    expect(isSome(value)).toBe(true);
 });
 
 test("ifSome works", () => {
-  let x = 0;
-  ifSome(Some(10), (v) => (x = v));
-  expect(x).toBe(10);
+    let x = 0;
+    ifSome(Some(10), (v) => (x = v));
+    expect(x).toBe(10);
 });
 
-// TODO: Add match test
+test("fromNullable null value returns none", () => {
+    const val = null;
+    const x = fromNullable(val);
+    expect(x.kind).toBe("None")
+})
+
